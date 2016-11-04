ebc83f31e86632c572bb95bbcab47a1e2fe6eb60
hesu <hesuslee@gmail.com>
2016-11-04 21:42:38 +0900

    modified:   sample/t.cpp

--- ds/sample/t.cpp
+++ ds/sample/t.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	clock_t begin = clock();
-	/* starting code */
+
+  /* starting code */
 	/* end of code */
+
 	clock_t end = clock();
 	std::cout << "elapsed time=" << double(end - begin) / CLOCKS_PER_SEC << std::endl;
 	return 0;
