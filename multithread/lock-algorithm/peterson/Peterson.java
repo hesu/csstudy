23b736c5ecf0ed5d35b440c2983602248fd7e15d
hesu <hesuslee@gmail.com>
2016-11-30 22:45:36 +0900

    add comment

--- multithread/lock-algorithm/peterson/Peterson.java
+++ multithread/lock-algorithm/peterson/Peterson.java
@@ -6,13 +6,13 @@ class Peterson implements Lock {
 	public void lock() {
 		int j = ThreadID.get();
 		int j = 1-i;
-		flag[i] = true;
-		victim = i;
-		while( flag[j] && victim == i) {};
+		flag[i] = true; // I'm interested
+		victim = i; // You go first
+		while( flag[j] && victim == i) {}; // wait
 	}
 
 	public void unlock() {
 		int i = ThreadID.get();
-		flag[i] = false;
+		flag[i] = false; // I'm not interested
 	}
 }
