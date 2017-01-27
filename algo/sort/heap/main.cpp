4205646b677074d33fbd8d6f3654f95224a9fefa
hesu <hesuslee@gmail.com>
2017-01-28 00:34:39 +0900

    heapsort done

--- algo/sort/heap/main.cpp
+++ algo/sort/heap/main.cpp
@@ -9,20 +9,68 @@
 
 using namespace std;
 
+void heapify( vector<int> &v, int idx, int max)
+{
+  int left = 2*idx + 1;
+  int right = 2*idx + 2;
+
+  int largest;
+  if (left < max && v[left] > v[idx]) {
+    largest = left;  
+  } else {
+    largest = idx;
+  }
+
+  if( right < max && v[right] > v[largest]) {
+    largest = right;
+  }
+
+  if( largest != idx) {
+    int swap = v[idx];
+    v[idx] = v[largest];
+    v[largest] = swap;
+    heapify( v, largest, max);
+  }
+
+}
+
+void buildheap( vector<int> &v)
+{
+  int max = v.size()-1;
+
+  for(int i=int(max/2)-1;i>=0; i--)
+  {
+    heapify( v, i, max);
+  }
+}
+
 int main(int argc, char** argv)
 {
 	clock_t begin = clock();
 	/* starting code */
 
   // Generate & Suffle
-  vector<int> bar;
-  for(int i=0; i<100; i++) bar.push_back(i);
-  random_shuffle( std::begin( bar ) , std::end( bar ));
+  vector<int> v;
+  for(int i=0; i<100; i++) v.push_back(i);
+  random_shuffle( std::begin( v) , std::end( v));
 
-  for(int i=0; i<100; i++) cout << bar[i] << " ";
+  for(int i=0; i<100; i++) cout << v[i] << " ";
+  cout << "\n" << endl;
+
+  // heap sort
+  buildheap(v);
+  for(int i=v.size()-1; i>=1; i--)
+  {
+    int swap = v[0];
+    v[0] = v[i];
+    v[i] = swap;
+    heapify( v, 0, i);
+  }
+
+
+  cout << "after sort : ";
+  for(int i=0; i<100; i++) cout << v[i] << " ";
   cout << endl;
-
-  // TODO heap sort;
 
 	/* end of code */
 	clock_t end = clock();
